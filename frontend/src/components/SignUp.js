65a3ce4baf3ce5edac41c1cca4fb123be0c56a5f
Ujjwal <122719755+ujjwal0414@users.noreply.github.com>
2023-10-13 20:58:34 +0530

    Update SignUp.js

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -21,7 +21,7 @@ const SignUp=()=>{
        }
        else{
         
-        result= await fetch(`${localConnection}/signup`,{
+        result= await fetch(`https://hostel-mangement-with-mern-stack-api.vercel.app/signup`,{
             method:"post",
             body:JSON.stringify({
                 username:userName,
