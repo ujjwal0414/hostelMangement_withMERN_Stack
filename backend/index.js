32d80342834673ead4a6a7f8330d337f7734c378
Ujjwal <122719755+ujjwal0414@users.noreply.github.com>
2023-10-13 21:54:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const messRating=require("./db/messRating");
 const problemSolverMail=require("./problemSolverMail");
 app.use(cor(
     origin: 'https://hostel-mangement-with-mern-stack.vercel.app', 
-  methods:'GET,POST,PUT,DELETE', 
+  methods:["get","put","post","delete"], 
   credentials: true, 
 ));
 app.get("/",(req,resp)=>{
