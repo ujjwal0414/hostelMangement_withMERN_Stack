74326c27f06aaa3f71aa098a2c2f646e1d081122
Ujjwal <122719755+ujjwal0414@users.noreply.github.com>
2023-10-27 14:42:14 +0530

    Update userProfilePic.js

--- backend/db/userProfilePic.js
+++ backend/db/userProfilePic.js
@@ -1,1 +1,7 @@
-
+const mongoose=require("mongoose");
+let userProfilePic=new mongoose.Schema({
+    UserfileName:String,
+    userNameprofile:String,
+    userEmailprofile:String
+});
+module.exports=mongoose.model("userprofile",userProfilePic);
